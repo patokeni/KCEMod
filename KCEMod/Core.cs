71dfe4a155114216088377490d428d7ef72d2f20
Lovely_xianxian <xyx_dada@outlook.com>
2025-03-26 21:01:39 +0800

    Initial commit

---
+++ KCEMod/Core.cs
@@ -0,0 +1,52 @@
+﻿using Il2CppInterop.Runtime.Injection;
+using KCEMod.Game;
+using MelonLoader;
+using System.Collections;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(KCEMod.Core), "KCEMod", "1.0.0", "xianxian", null)]
+[assembly: MelonGame("KAMITSUBAKI STUDIO", "KAMITSUBAKI CITY ENSEMBLE")]
+
+namespace KCEMod
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName.Equals("NewIdea2"))
+            {
+                MelonCoroutines.Start(WaitForGame());
+            }
+        }
+
+        IEnumerator WaitForGame()
+        {
+            GameObject player;
+            while (true)
+            {
+                player = GameObject.Find("Player");
+
+                if (player == null)
+                {
+                    yield return new WaitForSeconds(1F);
+                } else
+                {
+                    player.TryGetComponent<InputHandler>(out InputHandler handler);
+                    if (handler == null)
+                    {
+                        player.AddComponent<InputHandler>();
+                        LoggerInstance.Msg("We are playing");
+                    }
+
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ KCEMod/Patches/PatchGamemodeOnTouch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Il2Cpp;
+
+namespace KCEMod.Patches
+{
+    [HarmonyPatch]
+    public static class PatchGamemodeOnTouch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Gamemode1PlayerHandler), "Update", new Type[] { })]
+        private static void Patch_Update(Gamemode1PlayerHandler __instance)
+        {
+            // __instance.OnTouch();
+        }
+    }
+}
